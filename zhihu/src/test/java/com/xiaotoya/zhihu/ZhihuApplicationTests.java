13dec3494328e2c85198b84c245a57f6a06462f7
xiaotoya <996153422@qq.com>
2025-01-09 20:31:46 +0800

    passwordencoder init

--- zhihu/src/test/java/com/xiaotoya/zhihu/ZhihuApplicationTests.java
+++ zhihu/src/test/java/com/xiaotoya/zhihu/ZhihuApplicationTests.java
@@ -5,6 +5,7 @@ import com.xiaotoya.zhihu.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class ZhihuApplicationTests {
@@ -12,10 +13,20 @@ class ZhihuApplicationTests {
 	@Autowired
 	private UserMapper userMapper;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	void testFindByUsername() {
 		User user = userMapper.findByUsername("eric");
 		System.out.println(user);
+	}
+
+	@Test
+	void testPasswordEncoder() {
+		String encoder1 = passwordEncoder.encode("123");
+		System.out.println(encoder1);
+		System.out.println(passwordEncoder.matches("123", "$2a$10$tfE6QZHLG9YaI4ukAUS7teiriw9cbxqlPWycixsXVaCCK57IO9JwC"));
 	}
 
 }

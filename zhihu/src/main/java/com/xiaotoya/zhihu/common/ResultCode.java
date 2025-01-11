97a825ec1c3158b650611e83ec27a305fe2b6829
xiaotoya <996153422@qq.com>
2025-01-11 13:40:24 +0800

    refactor FOLDER

--- zhihu/src/main/java/com/xiaotoya/zhihu/common/ResultCode.java
+++
@@ -1,6 +0,0 @@
-package com.xiaotoya.zhihu.common;
-
-public interface ResultCode {
-    Integer SUCCESS = 0;
-    Integer FAIL = 1;
-}
--- zhihu/src/main/java/com/xiaotoya/zhihu/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.xiaotoya.zhihu.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.xiaotoya.zhihu.entities.User;
-
-public interface UserService {
-    public String login(User user) throws JsonProcessingException;
-}
